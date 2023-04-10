46ca9c6ec0cbac06ac5d63d1b6e6fd77b4e13b49
sonamdivyanshi1 <sonam.divyanshi@wisdmlabs.com>
2023-04-10 23:57:36 +0530

    plugin boilerplate structure

---
+++ includes/class-pickup-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://sonam.wisdmlabs.net
+ * @since      1.0.0
+ *
+ * @package    Pickup
+ * @subpackage Pickup/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Pickup
+ * @subpackage Pickup/includes
+ * @author     Sonam Divyanshi <sonam.divyanshi@wisdmlabs.com>
+ */
+class Pickup_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-pickup-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://sonam.wisdmlabs.net
+ * @since      1.0.0
+ *
+ * @package    Pickup
+ * @subpackage Pickup/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Pickup
+ * @subpackage Pickup/includes
+ * @author     Sonam Divyanshi <sonam.divyanshi@wisdmlabs.com>
+ */
+class Pickup_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
